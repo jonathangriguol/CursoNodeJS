9f83e60bd2a99d3287c990248ed9fdba3fe40d43
jonathangriguol <jonathan.griguol@gmail.com>
2024-05-28 01:43:57 -0300

    removing last commit

--- ecommerce/src/controllers/logger.controller.js
+++
@@ -1,8 +0,0 @@
-const loggerController = (req, res) => {
-  req.logger.warning("Alerta!");
-  req.logger.error("error");
-  res.send({ status: "success", message: "Probando Logger!" });
-};
-
-
-export default loggerController;
